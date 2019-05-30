57c5ee4f2fec073c37925a44a1ab112c1bfc4aea
Adam <adamlobocki@gmail.com>
2019-05-30 02:00:50 +0200

    Zadanie zrobione

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] numbersRandom = new int[10];
+        int x = numbersRandom.length - 1;
+
+        for (int i = 0; i < numbersRandom.length; i++) {
+            numbersRandom[i] = random.nextInt(500);
+        }
+
+        for (int i = 0; i <=numbersRandom.length * 2 - 1; i++) {
+            if(i<=9){
+                System.out.print(numbersRandom[i] + ", ");
+
+            }
+            else {
+                System.out.print(numbersRandom[x] + ", ");
+                x--;
+            }
+        }
+
+    }
+}
